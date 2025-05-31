8f130f9b1c1fa877b68e25cf0f21e01376c8c28a
Juan José Serna Pérez <juanjosesernaperez366@gmail.com>
2025-05-30 20:01:23 -0500

    Inicio Viajes

---
+++ entidades/gasto.py
@@ -0,0 +1,11 @@
+from datetime import date
+
+class Gasto:
+    def __init__(self):
+        self.__pais: str
+        self.__fecha_inicio: date
+        self.__fecha_fin: date
+        self.__presupuesto_diario: float
+        self.__moneda_iso: str
+    
+    
